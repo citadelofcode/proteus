63d20cbec56b1e22c0faf32cc1504115ff679516
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2024-09-19 00:35:30 +0530

    Created a new branch for development changes

--- main.go
+++ main.go
@@ -13,11 +13,13 @@ func main() {
 		fmt.Printf("Error occurred while creating web server: %s", err.Error())
 		os.Exit(1)
 	}
+
 	CurrentDirectory, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Error occurred while getting current working directory: " + err.Error())
 		os.Exit(1)
 	}
+	
 	TargetDirectory := filepath.Join(CurrentDirectory, "Files")
 	server.Static("/files", TargetDirectory)
 	server.Listen(8080)
