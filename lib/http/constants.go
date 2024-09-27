4f20a65cda595ee78a3d246bd22a32cad0e71098
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2024-09-27 16:04:31 +0530

    Added Last modified time to file metadata structure

--- lib/http/constants.go
+++ lib/http/constants.go
@@ -1,11 +1,14 @@
 package http
 
 const (
-	SERVER_NAME = "sparrow"
 	ERROR_MSG_CONTENT_TYPE = "text/html"
 	HEADER_LINE_SEPERATOR = "\r\n"
 	REQUEST_LINE_SEPERATOR = " "
 	HEADER_KEY_VALUE_SEPERATOR = ":"
-	DEFAULT_PORT = 8080
 	VALIDATE_ROUTE_PATTERN = "^[a-zA-z][a-zA-Z0-9_/:-]*$"
 )
+
+var ServerName string
+var DateHeaders []string
+var DefaultHostname string
+var DefaultPortNumber int
--- lib/http/server.go
+++ lib/http/server.go
@@ -33,13 +33,13 @@ func (srv *HttpServer) Static(Route string, TargetPath string) {
 
 func (srv * HttpServer) Listen(PortNumber int, HostAddress string) {
 	if PortNumber == 0 {
-		srv.PortNumber = srv.Config.GetDefaultPort();
+		srv.PortNumber = DefaultPortNumber
 	} else {
 		srv.PortNumber = PortNumber
 	}
 
 	if HostAddress == "" {
-		srv.HostAddress = srv.Config.GetDefaultHostname()
+		srv.HostAddress = DefaultHostname
 	} else {
 		srv.HostAddress = strings.TrimSpace(HostAddress)
 	}
@@ -69,78 +69,78 @@ func (srv *HttpServer) handleClient(ClientConnection net.Conn) {
 	defer ClientConnection.Close()
 	httpRequest := NewRequest(ClientConnection)
 	httpResponse := NewResponse(ClientConnection)
-	err := httpRequest.Read(srv.Config.GetAllowedHeaders())
+	err := httpRequest.read()
 	if err != nil {
 		srv.logError(err.Error())
-		httpResponse.Set(StatusInternalServerError, "", "", StatusInternalServerError.GetErrorContent())
+		httpResponse.set(StatusInternalServerError, "", "", StatusInternalServerError.GetErrorContent())
 		srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 		return
 	}
 
 	responseVersion := srv.getResponseVersion(httpRequest.Version)
-
+	
 	switch httpRequest.Method {
 	case "GET":
 		if !srv.Config.IsMethodAllowed(responseVersion, "GET") {
 			srv.logError("'GET' method is not allowed in HTTP version " + responseVersion)
-			httpResponse.Set(StatusMethodNotAllowed, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusMethodNotAllowed.GetErrorContent())
+			httpResponse.set(StatusMethodNotAllowed, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusMethodNotAllowed.GetErrorContent())
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 			return
 		}
 		TargetFilePath, staticRouteExists := srv.StaticRouter.Match(httpRequest.ResourcePath)
 		if !staticRouteExists {
 			srv.logError("A Static route matching the given resource does  not exist")
-			httpResponse.Set(StatusNotFound, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusNotFound.GetErrorContent())
+			httpResponse.set(StatusNotFound, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusNotFound.GetErrorContent())
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 			return
 		}
 		file, err := srv.getFile(TargetFilePath)
 		if err != nil {
 			srv.logError(err.Error())
-			httpResponse.Set(StatusInternalServerError, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusInternalServerError.GetErrorContent())
+			httpResponse.set(StatusInternalServerError, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusInternalServerError.GetErrorContent())
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 			return
 		}
 		if !httpRequest.isConditionalGet(file.LastModifiedAt) {
-			httpResponse.Set(StatusOK, responseVersion, file.ContentType, file.Contents)
+			httpResponse.set(StatusOK, responseVersion, file.ContentType, file.Contents)
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 		} else {
-			httpResponse.Set(StatusNotModified, responseVersion, file.ContentType, make([]byte, 0))
+			httpResponse.set(StatusNotModified, responseVersion, file.ContentType, make([]byte, 0))
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 		}
 	case "HEAD":
 		if !srv.Config.IsMethodAllowed(responseVersion, "HEAD") {
 			srv.logError("'HEAD' method is not allowed in HTTP version " + responseVersion)
-			httpResponse.Set(StatusMethodNotAllowed, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusMethodNotAllowed.GetErrorContent())
+			httpResponse.set(StatusMethodNotAllowed, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusMethodNotAllowed.GetErrorContent())
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 			return
 		}
 		TargetFilePath, staticRouteExists := srv.StaticRouter.Match(httpRequest.ResourcePath)
 		if !staticRouteExists {
 			srv.logError("A Static route matching the given resource does  not exist")
-			httpResponse.Set(StatusNotFound, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusNotFound.GetErrorContent())
+			httpResponse.set(StatusNotFound, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusNotFound.GetErrorContent())
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 			return
 		}
 		file, err := srv.getFile(TargetFilePath)
 		if err != nil {
 			srv.logError(err.Error())
-			httpResponse.Set(StatusInternalServerError, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusInternalServerError.GetErrorContent())
+			httpResponse.set(StatusInternalServerError, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusInternalServerError.GetErrorContent())
 			srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 			return
 		}
-		httpResponse.Set(StatusOK, responseVersion, file.ContentType, make([]byte, 0))
+		httpResponse.set(StatusOK, responseVersion, file.ContentType, make([]byte, 0))
 		srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 	default:
 		srv.logError("The HTTP method is not allowed by the server. Allowed Methods are - " + srv.Config.GetAllowedMethods(responseVersion))
-		httpResponse.Set(StatusMethodNotAllowed, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusMethodNotAllowed.GetErrorContent())
+		httpResponse.set(StatusMethodNotAllowed, responseVersion, ERROR_MSG_CONTENT_TYPE, StatusMethodNotAllowed.GetErrorContent())
 		httpResponse.Headers.Add("Allow", srv.Config.GetAllowedMethods(responseVersion))
 		srv.sendResponse(httpRequest, httpResponse, ClientConnection.RemoteAddr().String())
 	}
 }
 
 func (srv *HttpServer) sendResponse(httpRequest *HttpRequest, httpResponse *HttpResponse, ClientAddress string) {
-	err := httpResponse.Write()
+	err := httpResponse.write()
 	if err != nil {
 		srv.SrvLogger.Printf("Error occurred while sending response to client (%s): %s", ClientAddress, err.Error())
 	} else {
@@ -174,18 +174,16 @@ func (srv *HttpServer) getContentType(CompleteFilePath string) (string, error) {
 }
 
 func (srv *HttpServer) getFile(CompleteFilePath string) (*fs.File, error) {
-	var file fs.File
 	contentType, err := srv.getContentType(CompleteFilePath)
 	if err != nil {
 		return nil, err
 	}
-	fileContents, err := fs.ReadFileContents(CompleteFilePath)
+	
+	file, err := fs.GetFile(CompleteFilePath, contentType)
 	if err != nil {
 		return nil, err
 	}
-	file.Contents = fileContents
-	file.ContentType = contentType
-	return &file, nil
+	return file, nil
 }
 
 func (srv *HttpServer) getResponseVersion(requestVersion string) string {
