a5bb668cd7f6def9e4d064852ad4a5021b26e774
Mahesh Kumaar Balaji <maheshkumaar.balaji@outlook.com>
2024-10-07 11:17:14 -0700

    Reafctored routing logic and added POST method support for  HTTP/1.0

--- lib/http/server.go
+++ lib/http/server.go
@@ -21,7 +21,7 @@ type HttpServer struct {
 
 // Define a static route and map to a static file or folder in the file system.
 func (srv *HttpServer) Static(Route string, TargetPath string) {
-	err := srv.innerRouter.AddStatic(Route, TargetPath)
+	err := srv.innerRouter.addStaticRoute(Route, TargetPath)
 	if err != nil {
 		LogError(fmt.Sprintf("Adding Static Route to Server :: %s", err.Error()))
 		return
@@ -50,7 +50,7 @@ func (srv * HttpServer) Listen(PortNumber int, HostAddress string) {
 	}
 	srv.Socket = server
 	defer srv.Socket.Close()
-	LogError(fmt.Sprintf("Web server is listening at http://%s", serverAddress))
+	LogInfo(fmt.Sprintf("Web server is listening at http://%s", serverAddress))
 
 	for {
 		clientConnection, err := srv.Socket.Accept()
@@ -58,7 +58,7 @@ func (srv * HttpServer) Listen(PortNumber int, HostAddress string) {
 			LogError(fmt.Sprintf("Error occurred while accepting a new client: %s", err.Error()))
 			continue
 		}
-		LogError(fmt.Sprintf("A new client - %s has connected to the server", clientConnection.RemoteAddr().String()))
+		LogInfo(fmt.Sprintf("A new client - %s has connected to the server", clientConnection.RemoteAddr().String()))
 		go srv.handleClient(clientConnection)
 	}
 }
@@ -73,49 +73,73 @@ func (srv *HttpServer) handleClient(ClientConnection net.Conn) {
 	switch httpRequest.Method {
 	case "GET":
 		if !IsMethodAllowed(httpResponse.Version, "GET") {
-			LogError("'GET' method is not allowed in HTTP version " + httpResponse.Version)
 			httpResponse.Status(StatusMethodNotAllowed)
-			httpResponse.AddHeader("Allow", GetAllowedMethods(httpResponse.Version))
-			httpResponse.SendError(StatusMethodNotAllowed.GetErrorContent())
+			ErrorHandler(httpRequest, httpResponse)
 			return
 		}
-		TargetFilePath, staticRouteExists := srv.innerRouter.MatchStatic(httpRequest.ResourcePath)
-		if !staticRouteExists {
-			LogError("A Static route matching the given resource does  not exist")
-			httpResponse.Status(StatusNotFound)
-			httpResponse.SendError(StatusNotFound.GetErrorContent())
-			return
-		}
-
-		if !httpRequest.isConditionalGet(TargetFilePath) {
-			httpResponse.Status(StatusOK)
-			httpResponse.SendFile(TargetFilePath, false)
-		} else {
-			httpResponse.Status(StatusNotModified)
-			httpResponse.SendFile(TargetFilePath, true)
+		TargetFilePath, staticRouteExists := srv.innerRouter.matchStatic(httpRequest.ResourcePath)
+		if staticRouteExists {
+			httpRequest.staticFilePath = TargetFilePath
+			StaticFileHandler(httpRequest, httpResponse)
+			return	
 		}
 	case "HEAD":
 		if !IsMethodAllowed(httpResponse.Version, "HEAD") {
-			LogError("'HEAD' method is not allowed in HTTP version " + httpResponse.Version)
 			httpResponse.Status(StatusMethodNotAllowed)
-			httpResponse.AddHeader("Allow", GetAllowedMethods(httpResponse.Version))
-			httpResponse.SendError(StatusMethodNotAllowed.GetErrorContent())
+			ErrorHandler(httpRequest, httpResponse)
 			return
 		}
-		TargetFilePath, staticRouteExists := srv.innerRouter.MatchStatic(httpRequest.ResourcePath)
+		TargetFilePath, staticRouteExists := srv.innerRouter.matchStatic(httpRequest.ResourcePath)
 		if !staticRouteExists {
 			LogError("A Static route matching the given resource does  not exist")
 			httpResponse.Status(StatusNotFound)
-			httpResponse.SendError(StatusNotFound.GetErrorContent())
+			ErrorHandler(httpRequest, httpResponse)
 			return
 		}
 
-		httpResponse.Status(StatusOK)
-		httpResponse.SendFile(TargetFilePath, true)
+		httpRequest.staticFilePath = TargetFilePath
+		StaticFileHandler(httpRequest, httpResponse)
+	case "POST":
+		if !IsMethodAllowed(httpResponse.Version, "POST") {
+			httpResponse.Status(StatusMethodNotAllowed)
+			ErrorHandler(httpRequest, httpResponse)
+			return
+		}
 	default:
-		LogError("The HTTP method is not supported by the server. Allowed Methods are - " + GetAllowedMethods(httpResponse.Version))
 		httpResponse.Status(StatusMethodNotAllowed)
-		httpResponse.AddHeader("Allow", GetAllowedMethods(httpResponse.Version))
-		httpResponse.SendError(StatusMethodNotAllowed.GetErrorContent())
+		ErrorHandler(httpRequest, httpResponse)
+	}
+}
+
+// Creates a new GET endpoint at the given route path and sets the handler function to be invoked when the route is requested by the user.
+func (srv *HttpServer) Get(routePath string, handlerFunc Handler) {
+	routePath = strings.TrimSpace(routePath)
+	key := fmt.Sprintf("GET %s", routePath)
+	err := srv.innerRouter.addDynamicRoute(key, handlerFunc)
+	if err != nil {
+		LogError(fmt.Sprintf("Adding Dynamic Route to Server :: %s", err.Error()))
+		return
+	}
+}
+
+// Creates a new HEAD endpoint at the given route path and sets the handler function to be invoked when the route is requested by the user.
+func (srv *HttpServer) Head(routePath string, handlerFunc Handler) {
+	routePath = strings.TrimSpace(routePath)
+	key := fmt.Sprintf("HEAD %s", routePath)
+	err := srv.innerRouter.addDynamicRoute(key, handlerFunc)
+	if err != nil {
+		LogError(fmt.Sprintf("Adding Dynamic Route to Server :: %s", err.Error()))
+		return
+	}
+}
+
+// Creates a new POST endpoint at the given route path and sets the handler function to be invoked when the route is requested by the user.
+func (srv *HttpServer) Post(routePath string, handlerFunc Handler) {
+	routePath = strings.TrimSpace(routePath)
+	key := fmt.Sprintf("POST %s", routePath)
+	err := srv.innerRouter.addDynamicRoute(key, handlerFunc)
+	if err != nil {
+		LogError(fmt.Sprintf("Adding Dynamic Route to Server :: %s", err.Error()))
+		return
 	}
 }
